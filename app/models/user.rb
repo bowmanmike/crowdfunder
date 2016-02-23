24f4a8d64b79eafe9fb6516ffe60650b58c2978c
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-02-23 17:59:33 -0500

    install sorcery, modify users table column (password_digest => crypted_password) add column to users table (salt)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
   has_many :owned_projects, class_name: Project, foreign_key: 'owner_id'
   has_many :pledged_projects, through: :pledges, class_name: Project, foreign_key: 'project_id'
   has_many :pledges, foreign_key: 'backer_id'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223192904) do
+ActiveRecord::Schema.define(version: 20160223224803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,12 +55,13 @@ ActiveRecord::Schema.define(version: 20160223192904) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "crypted_password"
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "salt"
   end
 
   add_foreign_key "pledges", "projects"
