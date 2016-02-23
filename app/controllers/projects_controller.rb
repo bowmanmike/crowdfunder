7cd22697adbb4b28b8553b17329d26af2ebe1d8d
Mike Bowman <mbowman2@gmail.com>
2016-02-23 17:18:31 -0500

    Add all methods to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,24 @@
 class ProjectsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
 end
