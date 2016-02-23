20ee1651795d0f4d1c48bfc8cf1013dfec4a97e5
Mike Bowman <mbowman2@gmail.com>
2016-02-23 16:00:17 -0500

    Fix associations, all models work

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
-  has_many :backers, through: :pledges, class_name: 'User'
+  has_many :backers, through: :pledges, class_name: 'User', foreign_key: 'backer_id'
   has_many :pledges
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :owned_projects, class_name: Project, foreign_key: 'owner_id'
-  has_many :pledged_projects, class_name: Pledge, foreign_key: 'backer_id'
-  has_many :pledges, as: :backer
+  has_many :pledged_projects, through: :pledges, class_name: Project, foreign_key: 'project_id'
+  has_many :pledges, foreign_key: 'backer_id'
 end
