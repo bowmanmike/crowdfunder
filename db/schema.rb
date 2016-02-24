b64b2bfbd41df83868d68721d19065f2adc20ec5
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-02-23 20:03:04 -0500

    troubleshoot and fix login/logout functionality, add flash messages to application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223230032) do
+ActiveRecord::Schema.define(version: 20160223230033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,10 +69,14 @@ ActiveRecord::Schema.define(version: 20160223230032) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "salt"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
   add_foreign_key "pledges", "projects"
   add_foreign_key "pledges", "rewards"
