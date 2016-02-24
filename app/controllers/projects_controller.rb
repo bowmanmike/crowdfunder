6b359cea0100cc8c41649cced86038354777ecbf
Mike Bowman <mbowman2@gmail.com>
2016-02-23 19:58:22 -0500

    Edit projects form works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,7 +31,13 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
   end
 
   def destroy
