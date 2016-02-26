698a5331869854d16b263c60460d0098e9778333
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-02-26 15:36:40 -0500

    add feature image to project show view

--- app/uploaders/feature_image_uploader.rb
+++ app/uploaders/feature_image_uploader.rb
@@ -32,9 +32,12 @@ class FeatureImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
+
+  # Create different versions of your uploaded files:
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
